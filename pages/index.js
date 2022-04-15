09de420bc23bf65af9d3a5240f2a14fdee3ef4f9
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-15 14:04:58 +0530

    s1

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import { PowerBtn, YouTube } from '../Data/allsvg';
 const index = () => {
   return (
     <div>
-      {/* <YouTube width="100" height="100"/> */}
-      {/* <PowerBtn width="100" height="100"/> */}
+      
     </div>
   );
 }
