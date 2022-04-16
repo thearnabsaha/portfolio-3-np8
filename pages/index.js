481eb1d91a09a31cdad8691e9536d9fb6fcfd497
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-16 10:10:55 +0530

    s1

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ const index = () => {
     <>
       <div id="index" >
       <a href=""><PowerBtn className="power" /></a>
-        
+      <a href="" className="abt">About.</a>
+      <a href="" className="skl">My Skills.</a>
       </div>
     </>
   );
