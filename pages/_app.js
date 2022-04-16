8bbcdb39476ab5a4b752d9325388f571614fa36f
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-16 13:49:59 +0530

    s1

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '../styles/globals.scss'
 import Link from 'next/link';
+import { motion } from 'framer-motion'
 import { PowerBtn, YouTube, Github, Facebook,Twitter, YinYang } from '../Data/allsvg';
 
 function MyApp({ Component, pageProps }) {
   return <>
         <Link href="/"><PowerBtn className="power" /></Link>
+        <div className="icon">
+          <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1}}><Github className="git ic" /></motion.div></a>
+          <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.2}}><Twitter className="twt ic" /></motion.div></a>
+          <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.4}}><Facebook className="fb ic" /></motion.div></a>
+          <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.6}}><YouTube className="yt ic" /></motion.div></a>
+          <motion.div className="line" initial={{height:0}} animate={{height:150}}transition={{type:'spring', duration:1, delay:1.8}}></motion.div>
+        </div>
         <Component {...pageProps} />
   </>
 }
-
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,11 @@ const index = () => {
       <div id="index" >
         <Link href="/Components/About" ><motion.p className="abt" initial={{y:100}} animate={{y:-30,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> About.</motion.p></Link>
         <Link href="/Components/About" ><motion.p className="skl" initial={{y:100}} animate={{y:-30,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> My Skills.</motion.p></Link>
-        <Link href="/Components/About" ><motion.p className="say" initial={{y:-100,x:-15}} animate={{y:30,x:-40,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> Say Hi...</motion.p></Link>
+        <Link href={{pathname:"mailto:thearnabsaha201@gmail.com"}} target="_blank"><motion.p className="say" initial={{y:-100,x:-15}} animate={{y:30,x:-40,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> Say Hi...</motion.p></Link>
         <Link href="/Components/About" ><motion.p className="wrk" initial={{x:100,rotate:(90)}} animate={{x:-40,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> Work</motion.p></Link>
         <Link href="/Components/About" ><motion.p className="tas" initial={{y:-100,x:10}} animate={{y:0,x:10,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> TAS</motion.p></Link>
         <h6>click here!</h6>
         <a onClick={()=>{setClick(!click)}}><YinYang className="yin"/></a>
-        <div className="icon">
-          <a href=""><Github className="git ic" /></a>
-          <a href=""><Twitter className="twt ic" /></a>
-          <a href=""><Facebook className="fb ic" /></a>
-          <a href=""><YouTube className="yt ic" /></a>
-          <div className="line"></div>
-        </div>
       </div>
     </>
   );
