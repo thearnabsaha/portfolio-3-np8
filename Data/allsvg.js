382843f88f93e265e1f8816aa99ed25fc687394f
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-19 15:50:12 +0530

    s1

--- Data/allsvg.js
+++ Data/allsvg.js
@@ -97,7 +97,7 @@ export const YouTube = (props) => {
 
 export const YinYang = (props) => {
   return (
-    <motion.svg
+    <svg
       aria-hidden="true"
       data-prefix="fas"
       data-icon="yin-yang"
@@ -111,7 +111,7 @@ export const YinYang = (props) => {
         d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm0 376c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32zm0-128c-53.02 0-96 42.98-96 96s42.98 96 96 96c-106.04 0-192-85.96-192-192S141.96 64 248 64c53.02 0 96 42.98 96 96s-42.98 96-96 96zm0-128c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32z"
         initial={{pathLength:0}} animate={{pathLength:1, transition:{duration:10,ease:'easeInOut',pathLength: {type: "spring", duration: 1.5, bounce: 0 },}}}
       />
-    </motion.svg>
+    </svg>
   );
 };
 
