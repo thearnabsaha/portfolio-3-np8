ab10c25760c111904a564f3a159d0f8d8eed63c3
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-23 11:26:55 +0530

    s2

--- Components/EveryWhere.jsx
+++ Components/EveryWhere.jsx
@@ -1,17 +1,30 @@
 import Link from 'next/link';
 import { motion } from 'framer-motion'
+import {DarkTheme} from '../Data/themes';
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { PowerBtn, YouTube, Github, Facebook,Twitter, YinYang } from '../Data/allsvg';
+const Text = styled.h1`
+position: fixed;
+top: ${props => props.top};
+left: ${props => props.left};
+right: ${props => props.right};
+color:${props => `rgba(${props.theme.textRgba},0.1)`};
+font-size: calc(5rem + 5vw);
+z-index:0;
+
+`
 const EveryWhere = () => {
     return (
         <div>
              <Link href="/About" passHref><motion.p className="tas" initial={{y:-100,x:10}} animate={{y:0,x:10,transition: { type:'spring', duration: 1.5, delay:1}}} whileHover={{scale:1.1}} whileTap={{scale:.9}}> TAS</motion.p></Link>
             <div className="icon">
-            <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1}}><Github className="git ic" /></motion.div></a>
-            <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.2}}><Twitter className="twt ic" /></motion.div></a>
-            <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.4}}><Facebook className="fb ic" /></motion.div></a>
-            <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.6}}><YouTube className="yt ic" /></motion.div></a>
-            <motion.div className="line" initial={{height:0}} animate={{height:150}}transition={{type:'spring', duration:1, delay:1.8}}></motion.div>
-        </div>
+                <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1}}><Github className="git ic" /></motion.div></a>
+                <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.2}}><Twitter className="twt ic" /></motion.div></a>
+                <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.4}}><Facebook className="fb ic" /></motion.div></a>
+                <a href=""><motion.div initial={{scale:0}} animate={{scale:[0,1,1.5,1]}} transition={{type:'spring', duration:1, delay:1.6}}><YouTube className="yt ic" /></motion.div></a>
+                <motion.div className="line" initial={{height:0}} animate={{height:150}}transition={{type:'spring', duration:1, delay:1.8}}></motion.div>
+            </div>
         </div>
     );
 }
--- pages/About.jsx
+++ pages/About.jsx
@@ -1,43 +1,36 @@
 import { motion} from 'framer-motion'
 import EveryWhere from '../Components/EveryWhere';
 import SPMAN from '../public/assets/Images/spaceman.png';
+import {DarkTheme} from '../Data/themes';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
-
-const theme= {
-    dark: {
-      textColor:"#000",
-    },
-    light: {
-      textColor:"#fff",
-    },
-  };
-  const FullDiv = styled.div`
-  background-color: ${(props)=>props.theme.dark.textColor};
+const Box = styled.div`
+    background-color: ${props => props.theme.body};
+    color: ${props => props.theme.text};
 `;
 const About = () => {
     return (
-        <FullDiv>
-            <ThemeProvider theme={theme}>
-                <motion.div id="about" exit={{y:-1000,transition:{duration:.5}}}>
-                    <motion.h5 className='big' initial={{x:-1000}} animate={{x:0,transition:{duration:1,delay:.5,type:"spring"}}}>About</motion.h5>
-                    <EveryWhere/>
-                    <div className="container">
-                        <div className="left">
-                            <p>Im a front-end developer located in India. I love to create simple yet beautiful websites with great user experience.</p>
-                            <p>Im interested in the whole frontend stack Like trying new things and building great projects. Im an independent freelancer and blogger. I love to write blogs and read books.</p>
-                            <p>I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.</p>
+        <>
+            <ThemeProvider theme={DarkTheme}>
+                <Box>
+                    <motion.div id="about" exit={{y:-1000,transition:{duration:.5}}}>
+                        <motion.h5 className='big' initial={{x:-1000}} animate={{x:0,color:"#fcf6f431",transition:{duration:1,delay:.5,type:"spring"}}}>About</motion.h5>
+                        <EveryWhere/>
+                        <div className="container">
+                            <div className="left">
+                                <p>Im a front-end developer located in India. I love to create simple yet beautiful websites with great user experience.</p>
+                                <p>Im interested in the whole frontend stack Like trying new things and building great projects. Im an independent freelancer and blogger. I love to write blogs and read books.</p>
+                                <p>I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.</p>
+                            </div>
+                            <motion.div className="right">
+                                <motion.img src={SPMAN.src} alt=""  initial={{y:1000,x:1000}} animate={{y:-100,x:-30,transition:{duration:.8}}}/>
+                            </motion.div>
                         </div>
-                        <motion.div className="right">
-                            <motion.img src={SPMAN.src} alt=""  initial={{y:1000,x:1000}} animate={{y:-100,x:-30,transition:{duration:.8}}}/>
-                        </motion.div>
-                    </div>
-                </motion.div>
+                    </motion.div>
+                </Box>
             </ThemeProvider>
-        </FullDiv>
+        </>
     );
 }
 
 export default About;
-
-
