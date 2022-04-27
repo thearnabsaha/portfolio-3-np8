e8ccf5200d855d4b4da72bf7082aad9acdf89716
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-27 20:57:01 +0530

    s3

--- Components/WorkCard.jsx
+++ Components/WorkCard.jsx
@@ -8,7 +8,7 @@ const WorkCard = () => {
                     <h1>Agency Landing Page</h1>
                     <p>It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.</p>
                     <div id="hashtags">
-                        <p>#react #gsap #styledComponents</p>
+                        <h5>#react #gsap #styledComponents</h5>
                     </div>
                     <div className="button">
                         <a>Visit</a>
--- pages/Works.jsx
+++ pages/Works.jsx
@@ -1,10 +1,12 @@
 import EveryWhere from "../Components/EveryWhere";
+import EveryWhereDark from "../Components/EveryWhereDark";
 import WorkCard from "../Components/WorkCard";
 
 const Works = () => {
     return (
         <>
             <div id="works">
+            {/* <EveryWhereDark/> */}
             <EveryWhere/>
                 <WorkCard/>
             </div>
